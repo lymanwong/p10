3ddc5710be796bedb5aca2e74a7750a1c8f7367c
lymanwong <lymanwong@gmail.com>
2014-12-05 14:33:58 -0800

    even more shit

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,10 @@
+User.create(username: "Lyman", password: "123")
+
 10.times do
-  User.create(username: Faker::Name.name, password:
-    'boom')
+  User.create(username: Faker::Name.name, password: 'boom')
 
   array = [1,2,3]
-  array.sample
+  workoutName = ["Dumbell", "Jumping Jacks", "Sprints", "Curls"]
+  bodyPart = ["Arms", "Legs", "Back", "Neck", "Chest", "Abs"]
+  Workout.create(user_id: array.sample, name: workoutName.sample, body_part: bodyPart.sample, weight: rand(5..50), set: rand(1..5), rep: rand(1..5))
 end
